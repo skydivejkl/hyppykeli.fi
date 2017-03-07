0189383cc1ca90548f4af7c7134faadba0b79280
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-03-07 20:43:38 +0000

    Extract Parachute

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
-import simple, {css} from "react-simple";
+import simple from "react-simple";
 
 import {View} from "./core";
-import {addLatestGust} from "./LatestWindReadings";
+import Parachute from "./Parachute";
 
 const HeaderContainer = simple(View, {
     backgroundColor: "skyblue",
@@ -12,56 +12,6 @@ const HeaderContainer = simple(View, {
     marginBottom: 25,
     minHeight: 300,
 });
-
-const swing = css.keyframes({
-    "0%": {transform: "rotate(60deg)"},
-    "50%": {transform: "rotate(-60deg)"},
-    "100%": {transform: "rotate(60deg)"},
-});
-
-const rotate = css.keyframes({
-    "0%": {transform: "rotate(0deg)"},
-    "100%": {transform: "rotate(360deg)"},
-});
-
-const Parachute = simple(
-    View,
-    {
-        background: "url(/parachute.svg)",
-        backgroundSize: "contain",
-        width: 230,
-        backgroundRepeat: "no-repeat !important",
-        backgroundPosition: "center",
-        height: 250,
-        marginTop: 10,
-        opacity: 0.08,
-    },
-    {
-        swing: {
-            animation: `${swing} 2s ease-in-out infinite`,
-            transformOrigin: "50% 0%",
-        },
-        rotate: {
-            animation: `${rotate} 0.8s linear infinite`,
-            transformOrigin: "50% 15%",
-        },
-    }
-);
-
-var ConnectedParachute = ({value}) => {
-    // value = 8;
-    var gust = parseFloat(value, 10);
-    if (gust >= 11) {
-        return <Parachute rotate />;
-    }
-
-    if (gust >= 8) {
-        return <Parachute swing />;
-    }
-
-    return <Parachute />;
-};
-ConnectedParachute = addLatestGust(ConnectedParachute);
 
 const ParachuteContainer = simple(View, {
     position: "absolute",
@@ -75,7 +25,7 @@ const ParachuteContainer = simple(View, {
 const Header = ({children, ...props}) => (
     <HeaderContainer {...props}>
         <ParachuteContainer>
-            <ConnectedParachute />
+            <Parachute />
         </ParachuteContainer>
         {children}
     </HeaderContainer>
---
+++ src/Parachute.js
@@ -0,0 +1,57 @@
+import React from "react";
+import simple, {css} from "react-simple";
+
+import {View} from "./core";
+import {addLatestGust} from "./LatestWindReadings";
+
+const swing = css.keyframes({
+    "0%": {transform: "rotate(60deg)"},
+    "50%": {transform: "rotate(-60deg)"},
+    "100%": {transform: "rotate(60deg)"},
+});
+
+const rotate = css.keyframes({
+    "0%": {transform: "rotate(0deg)"},
+    "100%": {transform: "rotate(360deg)"},
+});
+
+const Parachute = simple(
+    View,
+    {
+        background: "url(/parachute.svg)",
+        backgroundSize: "contain",
+        width: 230,
+        backgroundRepeat: "no-repeat !important",
+        backgroundPosition: "center",
+        height: 250,
+        marginTop: 10,
+        opacity: 0.08,
+    },
+    {
+        swing: {
+            animation: `${swing} 2s ease-in-out infinite`,
+            transformOrigin: "50% 0%",
+        },
+        rotate: {
+            animation: `${rotate} 0.8s linear infinite`,
+            transformOrigin: "50% 15%",
+        },
+    }
+);
+
+var ConnectedParachute = ({value}) => {
+    // value = 8;
+    var gust = parseFloat(value, 10);
+    if (gust >= 11) {
+        return <Parachute rotate />;
+    }
+
+    if (gust >= 8) {
+        return <Parachute swing />;
+    }
+
+    return <Parachute />;
+};
+ConnectedParachute = addLatestGust(ConnectedParachute);
+
+export default ConnectedParachute;
