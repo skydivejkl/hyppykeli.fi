2f2cbbef5d848019d646e970ca16cb702fa13b4d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-03-09 23:07:04 +0000

    Drop manual start time

--- server/fmi.js
+++ server/fmi.js
@@ -51,8 +51,8 @@ router.get("/api/observations/:fmisid", async ctx => {
         query: "fmi::observations::weather::timevaluepair",
         cacheKey: "observations:" + ctx.params.fmisid,
         params: {
-            starttime: moment().subtract(6, "hours").toISOString(),
-            endtime: moment().toISOString(),
+            starttime: moment().subtract(8, "hours").toISOString(),
+            // endtime: moment().toISOString(),
             parameters: OBSERVATION_PARAMETERS,
             fmisid: ctx.params.fmisid,
         },
@@ -94,8 +94,8 @@ router.get("/api/observations/:fmisid/:feature", async ctx => {
         query: "fmi::observations::weather::timevaluepair",
         cacheKey: "observations:" + ctx.params.fmisid,
         params: {
-            starttime: moment().subtract(6, "hours").toISOString(),
-            endtime: moment().toISOString(),
+            starttime: moment().subtract(8, "hours").toISOString(),
+            // endtime: moment().toISOString(),
             parameters: OBSERVATION_PARAMETERS,
             fmisid: ctx.params.fmisid,
         },
