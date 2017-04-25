8576382862320059c42a8b6d5efec84f5eed6f8c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2017-04-25 18:25:12 +0000

    Use multiple link header for h2 push

--- server/h2push.js
+++ server/h2push.js
@@ -6,18 +6,15 @@ const h2pushJSON = path => `<${path}>; rel=preload; as=json`;
 router.get("/dz/:dz", (ctx, next) => {
     const dz = dropzones[ctx.params.dz.toUpperCase()];
 
-    ctx.set(
-        "Link",
-        [
-            `/api/observations/${dz.fmisid}/fi-1-1-windgust`,
-            `/api/observations/${dz.fmisid}/fi-1-1-windspeedms`,
-            `/api/forecasts/${dz.lat},${dz.lon}/enn-s-1-1-windgust`,
-            `/api/forecasts/${dz.lat},${dz.lon}/enn-s-1-1-windspeedms`,
-            `/api/metars/${dz.icaocode}`,
-        ]
-            .map(h2pushJSON)
-            .join(",")
-    );
+    [
+        `/api/observations/${dz.fmisid}/fi-1-1-windgust`,
+        `/api/observations/${dz.fmisid}/fi-1-1-windspeedms`,
+        `/api/forecasts/${dz.lat},${dz.lon}/enn-s-1-1-windgust`,
+        `/api/forecasts/${dz.lat},${dz.lon}/enn-s-1-1-windspeedms`,
+        `/api/metars/${dz.icaocode}`,
+    ].forEach(path => {
+        ctx.append("Link", h2pushJSON(path));
+    });
 
     next();
 });
