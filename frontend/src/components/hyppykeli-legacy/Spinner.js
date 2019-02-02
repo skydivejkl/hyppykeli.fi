c6d84042899ce7b493b2edf617a96c9d357963b6
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-02-02 15:03:57 +0200

    Simplify spinner

--- frontend/src/components/hyppykeli-legacy/Spinner.js
+++ frontend/src/components/hyppykeli-legacy/Spinner.js
@@ -1,30 +1,19 @@
 import "react-spinner/react-spinner.css";
-import simple, {css} from "react-simple";
-import cn from "classnames";
+
+import styled from "@emotion/styled";
+
 import ReactSpinner from "react-spinner";
-import {mapProps} from "recompose";
 
-css.global(".color-white .react-spinner_bar", {
-    backgroundColor: "white !important",
-});
-
-css.global(".color-black .react-spinner_bar", {
-    backgroundColor: "black !important",
-});
-
-css.global(".color-red .react-spinner_bar", {
-    backgroundColor: "red !important",
-});
-
-var Spinner = simple(ReactSpinner, {
-    width: "100% !important",
-    height: "100% !important",
-});
-Spinner = mapProps(({color, className, ...props}) => {
-    return {
-        ...props,
-        className: cn(className, "color-" + color),
-    };
-})(Spinner);
+const Spinner = styled(ReactSpinner)(
+    {
+        width: "100% !important",
+        height: "100% !important",
+    },
+    props => ({
+        ".react-spinner_bar": {
+            backgroundColor: `${props.color} !important`,
+        },
+    }),
+);
 
 export default Spinner;
