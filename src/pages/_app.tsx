cae49e61c3c878be328a1530e2ced65ba7762ba9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-03-08 19:55:14 +0200

    add next.js log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import "../styles.css";
 
 import React from "react";
 
+console.log("next.js version");
+
 export default function MyApp(props: { Component: any; pageProps: any }) {
     return <props.Component {...props.pageProps} />;
 }
